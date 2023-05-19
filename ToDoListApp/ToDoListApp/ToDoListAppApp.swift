df83522daf7b601c2c0f614c37b932a305272ea1
zahar.yashin <>
2023-05-19 10:19:06 +0700

    Create project

---
+++ ToDoListApp/ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Yashin Zahar on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
