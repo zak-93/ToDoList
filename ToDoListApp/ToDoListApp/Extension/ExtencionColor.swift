bd57b5c07a46d22571a4744af40b2382acca4cfe
zahar.yashin <>
2023-05-19 11:10:40 +0700

    Create model, icon app, custom color

---
+++ ToDoListApp/ToDoListApp/Extension/ExtencionColor.swift
@@ -0,0 +1,12 @@
+//
+//  ExtencionColor.swift
+//  ToDoListApp
+//
+//  Created by Yashin Zahar on 19.05.2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static let secondaryAccentColor = Color("SecondaryAccentColor")
+}
