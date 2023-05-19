72853841f6bea330544ca30031d2772ad2b8ab8d
zahar.yashin <>
2023-05-19 16:33:01 +0700

    Create ViewModel

---
+++ ToDoListApp/ToDoListApp/ViewModels/ListViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  ListViewModel.swift
+//  ToDoListApp
+//
+//  Created by Yashin Zahar on 19.05.2023.
+//
+
+import Foundation
+
+class ListViewModel: ObservableObject {
+    
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
+    
+    init() {
+        getItems()
+    }
+    
+    func getItems() {
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let saveItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = saveItems
+    }
+    
+    func deleteItem(_ indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    
+    func moveItem( indexSet: IndexSet,_ to: Int) {
+        items.move(fromOffsets: indexSet, toOffset: to)
+    }
+    
+    func addItem(title: String) {
+        let newItem = ItemModel(title: title, isCompleted: false)
+        items.append(newItem)
+    }
+    
+    func updateItem(item: ItemModel) {
+        if let index = items.firstIndex(where: {$0.id == item.id}) {
+            items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey )
+        }
+    }
+}
